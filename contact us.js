2d586325d24a16e46a9795f5a75f256f49830be4
chetana.nagare <chetananagare12@gmail.com>
2022-02-08 19:18:58 +0530

    first commit

---
+++ contact us.js
@@ -0,0 +1,6 @@
+function goback(){
+    document.getElementById("uname").value = "";
+    document.getElementById("email").value = "";    
+    document.getElementById("mobnum").value = "";
+    document.getElementById("comment").value = "";
+}
